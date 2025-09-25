e2b82adb45e204809c75a692706aa49f0338159c
Leandro74377 <leandrobaque4@gmail.com>
2025-09-25 20:41:00 +0000

    Corrijo index.html y agrego components tras merge

---
+++ Front/components/Courses.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Courses() {
+  return (
+    <div>
+      <h2>Cursos</h2>
+      <p>Aquí aparecerán los cursos del usuario.</p>
+    </div>
+  );
+}
